95845ac435932bb5115681d060056777c42f08e5
knknkn1162 <knknkn1162@gmail.com>
2017-10-14 00:40:26 +0900

    test loadump._is_binary

--- tests/test_loadump.py
+++ tests/test_loadump.py
@@ -1,1 +1,14 @@
+from anyloadump import loadump
+import unittest
 
+
+class LoadumpTests(unittest.TestCase):
+    def test_is_binary(self):
+        res = loadump._is_binary("./data/sample.json")
+        self.assertFalse(res)
+
+        res = loadump._is_binary("./data/sample.pickle")
+        self.assertTrue(res)
+
+        # ./data/dummy.pickle: cannot open `./data/dummy.pickle' (No such file or directory)
+        self.assertRaises(loadump.CharsetNotInferredError, lambda: loadump._is_binary("./data/dummy.pickle"))
