9318da1948cb2385ed6f77d4a9d3c293c141e925
knknkn1162 <knknkn1162@gmail.com>
2017-10-14 00:49:23 +0900

    test _extract_extension

--- tests/test_loadump.py
+++ tests/test_loadump.py
@@ -12,3 +12,10 @@ class LoadumpTests(unittest.TestCase):
 
         # ./data/dummy.pickle: cannot open `./data/dummy.pickle' (No such file or directory)
         self.assertRaises(loadump.CharsetNotInferredError, lambda: loadump._is_binary("./data/dummy.pickle"))
+
+    def test_extract_extension(self):
+        res = loadump._extract_extension("./data/sample.json")
+        self.assertEqual(res, "json")
+        res = loadump._extract_extension("./data/sample.pickle")
+        self.assertEqual(res, "pickle")
+        self.assertEqual(loadump._extract_extension("./data/dummy"), "")
