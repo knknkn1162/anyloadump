3af2521e4ec216087f1bc83d132766c80225db4e
knknkn1162 <knknkn1162@gmail.com>
2017-10-14 13:36:07 +0900

    add _LoadumpTests.get_path

--- tests/test_loadump.py
+++ tests/test_loadump.py
@@ -1,21 +1,34 @@
 from anyloadump import loadump
 import unittest
+import os
 
 
 class LoadumpTests(unittest.TestCase):
+    def _get_path(self, file):
+        root_dir = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(root_dir, file)
+
     def test_is_binary(self):
-        res = loadump._is_binary("./tests/data/sample.json")
+        import logging
+        logging.basicConfig(level=logging.ERROR)
+
+        logging.getLogger("anyloadump.loadump").setLevel(logging.DEBUG)
+
+        res = loadump._is_binary(self._get_path("data/sample.json"))
         self.assertFalse(res)
 
-        res = loadump._is_binary("./tests/data/sample.pickle")
+        res = loadump._is_binary(self._get_path("data/sample.pickle"))
         self.assertTrue(res)
 
         # ./data/dummy.pickle: cannot open `./data/dummy.pickle' (No such file or directory)
-        self.assertRaises(loadump.CharsetNotInferredError, lambda: loadump._is_binary("./tests/data/dummy.pickle"))
+        self.assertRaises(
+            loadump.CharsetNotInferredError,
+            lambda: loadump._is_binary(self._get_path("data/dummy.pickle"))
+        )
 
     def test_extract_extension(self):
-        res = loadump._extract_extension("./tests/data/sample.json")
+        res = loadump._extract_extension(self._get_path("data/sample.json"))
         self.assertEqual(res, "json")
-        res = loadump._extract_extension("./tests/data/sample.pickle")
+        res = loadump._extract_extension(self._get_path("data/sample.pickle"))
         self.assertEqual(res, "pickle")
-        self.assertEqual(loadump._extract_extension("./tests/data/dummy"), "")
+        self.assertEqual(loadump._extract_extension(self._get_path("data/dummy")), "")
