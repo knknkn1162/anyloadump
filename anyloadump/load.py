9f34c0b49f3614c2ea3ff8c6a316ad68afc43d4b
knknkn1162 <knknkn1162@gmail.com>
2017-10-14 20:27:02 +0900

    add buffering option from codecs.open option

--- anyloadump/load.py
+++ anyloadump/load.py
@@ -1,4 +1,7 @@
 from .loadump import loadump, DumpMode
 
-def load(file, *, encoding=None, errors=None, **kwargs):
-    return loadump(DumpMode.READ, file=file, encoding=encoding, errors=errors, **kwargs)
+def load(file, *, encoding=None, errors=None, buffering=None, **kwargs):
+    return loadump(DumpMode.READ, file=file, encoding=encoding, errors=errors, buffering=buffering, **kwargs)
+
+def loads(s, *, encoding=None, errors=None, buffering=None, **kwargs):
+    return load(DumpMode.READ, s=s, encoding=encoding, errors=errors, buffering=buffering, **kwargs)
