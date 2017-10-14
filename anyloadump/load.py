076e3a10b3742a653d5a8033095e0866ca1877f2
knknkn1162 <knknkn1162@gmail.com>
2017-10-14 21:16:33 +0900

    fix load.loads

--- anyloadump/load.py
+++ anyloadump/load.py
@@ -4,4 +4,4 @@ def load(filename, *, encoding=None, errors=None, buffering=None, **kwargs):
     return loadump(DumpMode.READ, filename=filename, encoding=encoding, errors=errors, buffering=buffering, **kwargs)
 
 def loads(s, *, encoding=None, errors=None, buffering=None, **kwargs):
-    return load(DumpMode.READ, s=s, encoding=encoding, errors=errors, buffering=buffering, **kwargs)
+    return loadump(DumpMode.READ, s=s, encoding=encoding, errors=errors, buffering=buffering, **kwargs)
