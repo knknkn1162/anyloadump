0014bcee86f78bc1d847576047c3e850c428ef0b
knknkn1162 <knknkn1162@gmail.com>
2017-10-14 11:09:27 +0900

    fix filepath in LoadumpTests

--- tests/test_loadump.py
+++ tests/test_loadump.py
@@ -4,18 +4,18 @@ import unittest
 
 class LoadumpTests(unittest.TestCase):
     def test_is_binary(self):
-        res = loadump._is_binary("./data/sample.json")
+        res = loadump._is_binary("./tests/data/sample.json")
         self.assertFalse(res)
 
-        res = loadump._is_binary("./data/sample.pickle")
+        res = loadump._is_binary("./tests/data/sample.pickle")
         self.assertTrue(res)
 
         # ./data/dummy.pickle: cannot open `./data/dummy.pickle' (No such file or directory)
         self.assertRaises(loadump.CharsetNotInferredError, lambda: loadump._is_binary("./data/dummy.pickle"))
 
     def test_extract_extension(self):
-        res = loadump._extract_extension("./data/sample.json")
+        res = loadump._extract_extension("./tests/data/sample.json")
         self.assertEqual(res, "json")
-        res = loadump._extract_extension("./data/sample.pickle")
+        res = loadump._extract_extension("./tests/data/sample.pickle")
         self.assertEqual(res, "pickle")
         self.assertEqual(loadump._extract_extension("./data/dummy"), "")
