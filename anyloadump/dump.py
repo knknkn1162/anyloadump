0222751bbef82a861075b78527c8f44011902b7b
knknkn1162 <knknkn1162@gmail.com>
2017-10-15 14:06:05 +0900

    add fmt option

--- anyloadump/dump.py
+++ anyloadump/dump.py
@@ -29,17 +29,17 @@ def xdump(obj, filename, *, encoding=None, errors=None, buffering=None, tbs=None
         **kwargs
     )
 
-def dumps(obj, *, encoding=None, errors=None, buffering=None, tbs=None, **kwargs):
+def dumps(obj, fmt, *, encoding=None, errors=None, buffering=None, tbs=None, **kwargs):
     ld = Loadumper(tbs)
-    return ld.loadump(obj=obj, dump_mode=DumpMode.WRITE,
-                   encoding=encoding, errors=errors, buffering=buffering, tbs=None, **kwargs)
-
-def adumps(obj, *, encoding=None, errors=None, buffering=None, tbs=None, **kwargs):
-    ld = Loadumper(tbs)
-    return ld.loadump(obj=obj, dump_mode=DumpMode.APPEND,
+    return ld.loadump(obj=obj, fmt=fmt, dump_mode=DumpMode.WRITE,
                    encoding=encoding, errors=errors, buffering=buffering, **kwargs)
 
-def xdumps(obj, *, encoding=None, errors=None, buffering=None, tbs=None, **kwargs):
+def adumps(obj, fmt, *, encoding=None, errors=None, buffering=None, tbs=None, **kwargs):
     ld = Loadumper(tbs)
-    return ld.loadump(obj=obj, dump_mode=DumpMode.EXCLUSIVE_CREATION,
+    return ld.loadump(obj=obj, fmt=fmt, dump_mode=DumpMode.APPEND,
+                   encoding=encoding, errors=errors, buffering=buffering, **kwargs)
+
+def xdumps(obj, fmt, *, encoding=None, errors=None, buffering=None, tbs=None, **kwargs):
+    ld = Loadumper(tbs)
+    return ld.loadump(obj=obj, fmt=fmt, dump_mode=DumpMode.EXCLUSIVE_CREATION,
                    encoding=encoding, errors=errors, buffering=buffering, **kwargs)
